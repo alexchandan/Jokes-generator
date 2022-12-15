90f2aea6002a4f1c284bbca566e5dac66699de31
Alex Chandan <chandanakela321@gmail.com>
2022-12-15 11:09:50 +0530

    error handlind added

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const parag = document.querySelector('.paragraph')
+const parag = document.querySelector('.paragraph');
+const nextBtn = document.querySelector('#next-btn');
+
 const jokes = async () => {
     try {
         const res = await fetch('https://hindi-jokes-api.onrender.com/jokes?api_key=b142593d620355dea7373cd9701f')
         const data = await res.json();
-        console.log(data);
         parag.innerHTML = data.jokeContent;
     } catch (error) {
         console.log(error)
+        parag.innerHTML = "OOPs...! Something Went Wrong!"
     }
 }
-
 jokes();
-
+nextBtn.addEventListener('click', jokes)
